474354a08bb251d1d926d9ae101824b4577fefbf
Rhet Turnbull <rturnbull@gmail.com>
2024-04-01 09:30:09 -0700

    Added build scripts

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script is very specific to my particular setup on my machine.
+# It must be run after `flit publish` to update the package on PyPI.
+# It uses `scripts/pyapp-runner.sh`, a simple CI script that runs via ssh,
+# to build and sign the binaries for the package and then build the installer package.
+
+# Get the current version of the package from the source
+VERSION=$(grep __version__ makelive/version.py | cut -d "\"" -f 2)
+
+# verify VERSION is valid
+# PyApp will happily build with an invalid version number
+# get directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYPI_VERSION=$(python $DIR/scripts/get_latest_pypi_version.py makelive)
+if [ "$PYPI_VERSION" != "$VERSION" ]; then
+    echo "Invalid version number: $VERSION"
+    echo "Latest version on PyPI: $PYPI_VERSION"
+    echo "Did you forget to run 'flit publish'?"
+    exit 1
+fi
+
+# Build the binaries and package them
+# arm64 binary built on a remote M1 Mac
+echo "Building version $VERSION for Apple Silicon"
+bash scripts/pyapp-runner.sh m1 makelive $VERSION
+
+echo "Building version $VERSION for Intel"
+bash scripts/pyapp-runner.sh macbook makelive $VERSION
---
+++ scripts/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Preinstall script for makelive installer
+# This will remove the makelive binary from /usr/local/bin if it exists
+
+rm -f /usr/local/bin/makelive
